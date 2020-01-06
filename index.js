cfef55a90d4e1ff77b942b4ce0ce3d73c8381fcf
Javier Devesa <javier.devesa@delonia.com>
2020-01-06 23:23:34 +0100

    uibuild

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get('/api/persons/:id', (req, res, next) => {
 app.delete('/api/persons/:id', (req, res, next) => {
   Phone.findByIdAndRemove(req.params.id)
   .then(result => {
-    response.status(204).end()
+    res.status(204).end()
   })
   .catch(error => next(error))
 })
